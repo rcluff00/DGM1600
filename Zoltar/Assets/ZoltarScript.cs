59570b7ff9853a6542649c515b91eec84c5bfa3f
Ryan Cluff <ryan.s.cluff@gmail.com>
2017-08-31 12:08:14 -0600

    Created Zoltar Guessing Game
    Unity Files

---
+++ Zoltar/Assets/ZoltarScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoltarScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Please allow me to introduce myself. My name is Zoltar, the mind reader.");
+        print("Please, in your mind's eye, pick a number between 1 and 100.");
+
+        print("Do tell, is your number higher or lower than 50? ");
+        print("(UP arrow for higher, DOWN arrow for lower, or ENTER for equals)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
