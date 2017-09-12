8e15345cc4c02d0b314aaee4e5ba6fdd6a59ee9c
Ryan Cluff <ryan.s.cluff@gmail.com>
2017-09-11 22:14:58 -0600

    Else assignment
    Updated file for Else Clause assignment

--- IfAndElse.cs
+++ IfAndElse.cs
@@ -20,6 +20,17 @@ namespace ConsoleApplication1
              */
 
             int x = 3;
+            double y = 4.2;
+
+            char input;
+
+            int hp;
+            int ap;
+
+            int remainder;
+            const int HALF = 2;
+
+            // 1 --------------------------------------------------------------------------------------------------
 
             if (x > 0)
             {
@@ -38,6 +49,55 @@ namespace ConsoleApplication1
                 {
                     Console.WriteLine("X is 0");
                 }
+            }
+
+            // 2 -------------------------------------------------------------------------------------------------
+
+            if (y % 2 == 0)
+            {
+                Console.WriteLine("Y is a whole number");
+            }
+
+            else
+            {
+                Console.WriteLine("Y is not a whole number");
+            }
+
+            // 3 -------------------------------------------------------------------------------------------------
+
+            Console.WriteLine("What day of the week is it today?");
+            if (input = 'M' || 'T' || 'W' || 'R' || 'F')
+            {
+                Console.WriteLine("It's not the weekend yet...you have to go to work");
+            }
+
+            else
+            {
+                Console.WriteLine("It's the weekend! You can go back to bed");
+            }
+
+            // 4 -------------------------------------------------------------------------------------------------
+
+            if (hp < ap)
+            {
+                Console.WriteLine("Your health points were depleted by the attacker...you died.");
+            }
+
+            else
+            {
+                Console.WriteLine("You live to fight another day");
+            }
+
+            // 5 ----------------------------------------------------------------------------------------------------
+
+            if (x % HALF == 0)
+            {
+                Console.WriteLine("X is an even number");
+            }
+
+            else
+            {
+                Console.WriteLine("X is aan odd number");
             }
         }
     }
