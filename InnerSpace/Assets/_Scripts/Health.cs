6c7e1816ad3a61751a82ad7451a40ad2c08cbc1a
Ryan Cluff <ryan.s.cluff@gmail.com>
2017-11-28 11:22:47 -0700

    Explosion Effect
    Created a pretty slick explosion effect using a particle system

---
+++ InnerSpace/Assets/_Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+    public GameObject explosionEffect;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0)
+        {
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        }
+	}
+}
--- InnerSpace/Assets/_Scripts/Meteor.cs
+++ InnerSpace/Assets/_Scripts/Meteor.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Meteor : MonoBehaviour {
 
     public float InitialRotationSpeed;
-    public int health;
 
 	void Start () {
         GetComponent<Rigidbody2D>().AddTorque(Random.Range(-InitialRotationSpeed, InitialRotationSpeed), ForceMode2D.Impulse);
